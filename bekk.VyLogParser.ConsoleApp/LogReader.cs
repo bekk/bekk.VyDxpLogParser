60eb7fd67cf0e6e9a393253c786fd5a8e5ed48ab
Øistein Hay Syversen <oistein.h.syversen@gmail.com>
2022-04-29 12:24:21 +0200

    Add project files.

---
+++ bekk.VyLogParser.ConsoleApp/LogReader.cs
@@ -0,0 +1,83 @@
+﻿using System.IO.Compression;
+
+namespace bekk.VyLogParser.ConsoleApp;
+
+public class LogReader
+{
+    public List<LogItem> Execute(string sourceFile, string destinationFolder)
+    {
+        if (Directory.Exists(destinationFolder))
+        {
+            Directory.Delete(destinationFolder, true);
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+        }
+
+        ZipFile.ExtractToDirectory(sourceFile, destinationFolder, true);
+
+        var files = Directory.GetFiles(destinationFolder, "*.csv", SearchOption.AllDirectories);
+
+        var logItems = new List<LogItem>();
+        foreach (var file in files)
+        {
+            using var reader = new StreamReader(file);
+            LogItem logItem = null;
+
+            while (!reader.EndOfStream)
+            {
+                var data = reader.ReadLine();
+                if (data == null) continue;
+                var temp = data.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                if (temp[0] == "date") continue;
+
+                if (DateTime.TryParse(temp[0], out var date))
+                {
+                    if (logItem != null)
+                    {
+                        logItems.Add(logItem);
+                    }
+
+                    logItem = new LogItem
+                    {
+                        Date = date,
+                        Level = temp[1],
+                        ApplicationName = temp[2],
+                        InstanceId = temp[3],
+                        EventTickCount = temp[4],
+                        EventId = temp[5],
+                        PId = temp[6],
+                        Tid = temp[7],
+                        Message = temp[8].Replace("\"", string.Empty) + "\r\n",
+                        Title = temp[8].Replace("\"", string.Empty)
+                    };
+                    continue;
+                }
+
+                if (logItem != null)
+                    logItem.Message += data + "\r\n";
+            }
+
+            if (logItem != null)
+            {
+                logItems.Add(logItem);
+            }
+        }
+
+        return logItems.OrderBy(x => x.Date).ToList();
+    }
+}
+
+public class LogItem
+{
+    public DateTime Date { get; set; }
+    public string Level { get; set; } = null!;
+    public string ApplicationName { get; set; } = null!;
+    public string InstanceId { get; set; } = null!;
+    public string EventTickCount { get; set; } = null!;
+    public string EventId { get; set; } = null!;
+    public string PId { get; set; } = null!;
+    public string Tid { get; set; } = null!;
+    public string Message { get; set; } = null!;
+    public string Title { get; set; } = null!;
+
+}
---
+++ bekk.VyLogParser.ConsoleApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection;
+using bekk.VyLogParser.ConsoleApp;
+using Newtonsoft.Json;
+
+Console.WriteLine($"Simple EPi log parser v{Assembly.GetExecutingAssembly().GetName().Version}\n");
+
+var arguments = Environment.GetCommandLineArgs().ToList();
+
+if (arguments.Count == 1)
+{
+    Console.WriteLine("Missing arguments!");
+    Console.WriteLine("Example: bekk.VyLogParser.ConsoleApp \"c:\\downloads\\source.zip\" \"c:\\destinationFolder\"");
+    return;
+}
+
+var zipPath = arguments.Skip(1).Take(1).FirstOrDefault();
+
+if (string.IsNullOrWhiteSpace(zipPath))
+{
+    Console.WriteLine("Missing zip file argument, aborting!");
+    return;
+}
+
+var extractPath = arguments.Skip(2).Take(1).FirstOrDefault() ?? @"c:\temp\Destination";
+
+var reader = new LogReader();
+var result = reader.Execute(zipPath, extractPath);
+
+Console.WriteLine($"Total log entries {result.Count}\n");
+
+foreach (var message in result.GroupBy(x => x.Title).OrderByDescending(x => x.Count()))
+{
+    var count = message.Count().ToString().PadLeft(5, ' ');
+    Console.WriteLine($"{count} {message.Key}");
+}
+
+TextWriter? writer = null;
+try
+{
+    Console.WriteLine($"Writing AllLogItems.json");
+    var contentsToWriteToFile = JsonConvert.SerializeObject(result);
+    writer = new StreamWriter(Path.Combine(extractPath, "AllLogItems.json"));
+    writer.Write(contentsToWriteToFile);
+}
+finally
+{
+    writer?.Close();
+}
