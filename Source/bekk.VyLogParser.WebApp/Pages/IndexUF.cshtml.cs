4c55ce170aa25e9b1e7b9643d5984fa54254d473
Øistein Hay Syversen <oistein.h.syversen@gmail.com>
2022-05-13 08:41:18 +0200

    Add drag and drop

---
+++ Source/bekk.VyLogParser.WebApp/Pages/IndexUF.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace bekk.VyLogParser.WebApp.Pages
+{
+    public class IndexUFModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Vy Log Parser
+
+### Drag and drop
+https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/File_drag_and_drop
