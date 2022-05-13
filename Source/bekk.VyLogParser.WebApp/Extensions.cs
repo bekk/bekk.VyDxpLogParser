951d295ecdf9c77ff2dcb4dfc9da8d9619295f3f
Øistein Hay Syversen <oistein.h.syversen@gmail.com>
2022-05-13 19:01:27 +0200

    Add fire and forget feature

---
+++ Source/bekk.VyLogParser.WebApp/Extensions.cs
@@ -0,0 +1,16 @@
+﻿namespace bekk.VyLogParser.WebApp;
+
+public static class Extensions
+{
+    public static async void FireAndForget(this Task task)
+    {
+        try
+        {
+            await task;
+        }
+        catch (Exception)
+        {
+            // log errors
+        }
+    }
+}
