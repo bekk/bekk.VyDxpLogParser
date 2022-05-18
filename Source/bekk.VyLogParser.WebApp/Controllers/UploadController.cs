b36306991f90c13304fe9cbd48d8b0d37696d255
Øistein Hay Syversen <oistein.h.syversen@gmail.com>
2022-05-18 10:48:45 +0200

    Better error handling when uploading

--- Source/bekk.VyLogParser.WebApp/Controllers/UploadController.cs
+++ Source/bekk.VyLogParser.WebApp/Controllers/UploadController.cs
@@ -8,30 +8,44 @@ namespace bekk.VyLogParser.WebApp.Controllers;
 [ApiController]
 public class UploadController : Controller
 {
+    private readonly ILogger<UploadController> _logger;
+
+    public UploadController(ILogger<UploadController> logger)
+    {
+        _logger = logger;
+    }
+
     [HttpPost(Name = "UploadFile")]
     public async Task<IActionResult> UploadFile(IFormFile file)
     {
-        var workFolder = $"wwwroot\\Upload\\{DateTime.Now.Ticks}";
-        var rootFolder = Path.Combine(Directory.GetCurrentDirectory(), workFolder);
-        var fileNameOnly = await FileHelper.WriteFile(file, rootFolder);
-        var fileName = new FileInfo(Path.Combine(rootFolder, fileNameOnly));
-        var arguments = new Arguments
+        try
         {
-            ClearOutputDirectory = false,
-            OutputDirectory = fileName.Directory!,
-            ZipFile = fileName
-        };
-        var logItems = LogReader.Execute(arguments);
-        var result = LogHelper.Execute(arguments, logItems);
+            var workFolder = $"wwwroot\\Upload\\{DateTime.Now.Ticks}";
+            var rootFolder = Path.Combine(Directory.GetCurrentDirectory(), workFolder);
+            var fileNameOnly = await FileHelper.WriteFile(file, rootFolder);
+            var fileName = new FileInfo(Path.Combine(rootFolder, fileNameOnly));
+            var arguments = new Arguments
+            {
+                ClearOutputDirectory = false,
+                OutputDirectory = fileName.Directory!,
+                ZipFile = fileName
+            };
+            var logItems = LogReader.Execute(arguments);
+            var result = LogHelper.Execute(arguments, logItems);
 
-        result.ResultAsLogItemsFile = FileHelper.ConvertPhysicalFileToWebFile(result.ResultAsLogItemsFile, workFolder);
-        result.ResultsAsJsonFile = FileHelper.ConvertPhysicalFileToWebFile(result.ResultsAsJsonFile, workFolder);
-        result.ResultAsSummaryFile = FileHelper.ConvertPhysicalFileToWebFile(result.ResultAsSummaryFile, workFolder);
+            result.ResultAsLogItemsFile = FileHelper.ConvertPhysicalFileToWebFile(result.ResultAsLogItemsFile, workFolder);
+            result.ResultsAsJsonFile = FileHelper.ConvertPhysicalFileToWebFile(result.ResultsAsJsonFile, workFolder);
+            result.ResultAsSummaryFile = FileHelper.ConvertPhysicalFileToWebFile(result.ResultAsSummaryFile, workFolder);
 
-        Task.Run(() => { FileHelper.CleanupOldArchives(rootFolder); }).FireAndForget();
+            Task.Run(() => { FileHelper.CleanupOldArchives(rootFolder); }).FireAndForget();
 
-        return Ok(result);
+            return Ok(result);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "En error has occurred when uploading file");
+            return BadRequest(e.Message);
+        }
     }
-
 
 }
